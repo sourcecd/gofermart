06db5957d697cbd08d16d66e0aebd33d1baddd51
Sergey J <wint@yandex-team.ru>
2024-06-04 01:44:49 +0300

    add list rows

--- internal/models/order.go
+++ internal/models/order.go
@@ -1,8 +1,8 @@
 package models
 
 type Order struct {
-	Number     int    `json:"number"`
-	Status     string `json:"status"`
-	Accrual    *int   `json:"accrual,omitempty"`
-	UploadedAt string `json:"uploaded_at"`
+	Number     int     `json:"number"`
+	Status     *string `json:"status,omitempty"`
+	Accrual    *int    `json:"accrual,omitempty"`
+	UploadedAt string  `json:"uploaded_at"`
 }
