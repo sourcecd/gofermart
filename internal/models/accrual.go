06b8aaf6808efe8335fd6affb915baed73d182f8
Sergey J <wint@yandex-team.ru>
2024-06-08 05:47:32 +0300

    tests

--- internal/models/accrual.go
+++ internal/models/accrual.go
@@ -1,7 +1,7 @@
 package models
 
 type Accrual struct {
-	Order   int64  `json:"order"`
+	Order   string  `json:"order"`
 	Status  string `json:"status"`
 	Accrual *int64 `json:"accrual,omitempty"`
 }
