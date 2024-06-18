27a2001189741b3c553fed2ef9a2080abdc00683
Sergey J <wint@yandex-team.ru>
2024-06-19 01:34:23 +0300

    fix withdrawals

--- internal/models/withdraw.go
+++ internal/models/withdraw.go
@@ -4,3 +4,9 @@ type Withdraw struct {
 	Order string  `json:"order"`
 	Sum   float64 `json:"sum"`
 }
+
+type Withdrawals struct {
+	Order       string  `json:"order"`
+	Sum         float64 `json:"sum"`
+	ProcessedAt string  `json:"processed_at"`
+}
--- internal/models/withdrawals.go
+++
@@ -1,7 +0,0 @@
-package models
-
-type Withdrawals struct {
-	Order       string  `json:"order"`
-	Sum         float64 `json:"sum"`
-	ProcessedAt string  `json:"processed_at"`
-}
