a55efe21d7e479cc8ffdb3bad233f819a26888b0
Sergey J <wint@yandex-team.ru>
2024-06-18 23:46:35 +0300

    fix method names

--- internal/compression/compression.go
+++ internal/compression/compression.go
@@ -10,57 +10,57 @@ import (
 
 var allowedCompressTypes = []string{"text/html", "application/json"}
 
-type compressWriter struct {
+type gzipResponseWriter struct {
 	w  http.ResponseWriter
 	zw *gzip.Writer
 }
 
-func NewCompressWriter(w http.ResponseWriter) *compressWriter {
-	return &compressWriter{
+func NewCompressWriter(w http.ResponseWriter) *gzipResponseWriter {
+	return &gzipResponseWriter{
 		w:  w,
 		zw: gzip.NewWriter(w),
 	}
 }
 
-func (c *compressWriter) Header() http.Header {
+func (c *gzipResponseWriter) Header() http.Header {
 	return c.w.Header()
 }
 
-func (c *compressWriter) Write(p []byte) (int, error) {
+func (c *gzipResponseWriter) Write(p []byte) (int, error) {
 	return c.zw.Write(p)
 }
 
-func (c *compressWriter) WriteHeader(statusCode int) {
+func (c *gzipResponseWriter) WriteHeader(statusCode int) {
 	c.w.Header().Set("Content-Encoding", "gzip")
 	c.w.WriteHeader(statusCode)
 }
 
-func (c *compressWriter) Close() error {
+func (c *gzipResponseWriter) Close() error {
 	return c.zw.Close()
 }
 
-type compressReader struct {
+type gzipReadCloser struct {
 	r  io.ReadCloser
 	zr *gzip.Reader
 }
 
-func NewCompressReader(r io.ReadCloser) (*compressReader, error) {
+func NewCompressReader(r io.ReadCloser) (*gzipReadCloser, error) {
 	zr, err := gzip.NewReader(r)
 	if err != nil {
 		return nil, err
 	}
 
-	return &compressReader{
+	return &gzipReadCloser{
 		r:  r,
 		zr: zr,
 	}, nil
 }
 
-func (c *compressReader) Read(p []byte) (n int, err error) {
+func (c *gzipReadCloser) Read(p []byte) (n int, err error) {
 	return c.zr.Read(p)
 }
 
-func (c *compressReader) Close() error {
+func (c *gzipReadCloser) Close() error {
 	if err := c.r.Close(); err != nil {
 		return err
 	}
